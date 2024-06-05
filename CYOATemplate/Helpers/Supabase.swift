77b9356225d852195a28c16947cef595d2ae6a77
David Li <dtli@lcs.on.ca>
2024-06-05 10:16:15 -0400

    Initial Commit

---
+++ CYOATemplate/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabase.swift
+//  CYOATemplate
+//
+//  Created by Russell Gordon on 2024-06-02.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://eiuwkhxgtvggjbirsvsu.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpdXdraHhndHZnZ2piaXJzdnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0MjgwMjcsImV4cCI6MjAzMzAwNDAyN30.A1jVXOdEoiL6FoxqFq7dgTovkTVtKUFf0mk3CJAGM7Y"
+)
+
