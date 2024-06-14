7a16068aec655c1a05bf920306ee3541cf890108
Justin Wei <jwei@lcs.on.ca>
2024-06-14 15:42:10 -0400

    Made it so that the prompts turn gray if they have been selected before.

---
+++ CYOATemplate/Views/EdgesChoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  EdgesChoiceView.swift
+//  CYOATemplate
+//
+//  Created by Justin Zack Wei on 2024-06-14.
+//
+
+import SwiftUI
+
+struct EdgesChoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EdgesChoiceView()
+}
